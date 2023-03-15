b4d4da758f4d2cfc9fd43514771b9e282f31d908
felsenfest7 <92101782+felsenfest7@users.noreply.github.com>
2023-03-15 18:13:24 +0300

    Add files via upload

---
+++ xtrack_istasyonlar/erdemli_xtrack.py
@@ -0,0 +1,67 @@
+from netCDF4 import Dataset as dt
+import xarray as xr
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import numpy as np
+import geopy.distance
+
+#Dosyanın konumu
+import sys
+sys.path.insert(1, "/home/furkan/PycharmProjects/pythonProject/venv/ALTIMETRY_PY/GENEL_DOSYALAR")
+
+import xtrack_functions as xf
+import xtrack_plot as xp
+
+desired_width=320
+pd.set_option('display.width', desired_width)
+np.set_printoptions(linewidth=desired_width)
+pd.set_option('display.max_columns',30)
+pd.set_option('display.max_rows',500000)
+#-----------------------------------------------------------------------------------------------------------------------
+#ERDEMLİ
+lrm_data = "/home/furkan/deus/ALTIMETRY/processler/XTRACK/XTRACK_DATA/ctoh.sla.ref.TP+J1+J2+J3.medsea.068.nc"
+lrm_dataset = xf.read_xtrack(lrm_data, 36.5637203, 34.25539255, 0.05)
+lrm_dataset_aylik = xf.aylik(lrm_dataset)
+
+#IQR HESABI
+filtered = xf.iqr_xtrack(lrm_dataset_aylik)
+
+#Dates interpolation
+filtered = xf.dates_interpolation_xtrack(filtered)
+
+plot = xp.plot_xtrack(filtered, "Erdemli X-TRACK Verileri", "2003-05-14")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
